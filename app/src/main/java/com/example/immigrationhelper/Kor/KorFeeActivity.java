702d2f4ef3e076d9e56dead23f1c691cf1c64c6c
Kimdeokki <9718kim@naver.com>
2021-05-06 16:19:52 +0900

    5/6 비자 정리

--- app/src/main/java/com/example/immigrationhelper/Kor/KorFeeActivity.java
+++ app/src/main/java/com/example/immigrationhelper/Kor/KorFeeActivity.java
@@ -3,20 +3,20 @@ package com.example.immigrationhelper.Kor;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.immigrationhelper.R;
 
-public class KorC4Activity extends AppCompatActivity {
+public class KorFeeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.korc4);
+        setContentView(R.layout.kor_fee);
 
-        Button kmainbtn = (Button) findViewById(R.id.kormainbtn);
+        ImageButton kmainbtn = (ImageButton) findViewById(R.id.kormainbtn);
 
         kmainbtn.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/immigrationhelper/Kor/KorVisa/KorB2Activity.java
+++ app/src/main/java/com/example/immigrationhelper/Kor/KorVisa/KorB2Activity.java
@@ -1,13 +1,15 @@
-package com.example.immigrationhelper.Kor;
+package com.example.immigrationhelper.Kor.KorVisa;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.immigrationhelper.Kor.KorActivity;
+import com.example.immigrationhelper.Kor.KorFeeActivity;
 import com.example.immigrationhelper.R;
 
 public class KorB2Activity extends AppCompatActivity {
@@ -16,12 +18,20 @@ public class KorB2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.korb2);
 
-        Button kmainbtn = (Button) findViewById(R.id.kormainbtn);
+        ImageButton kmainbtn = (ImageButton) findViewById(R.id.kormainbtn);
+        ImageButton kfeebtn = (ImageButton) findViewById(R.id.korfeebtn);
 
         kmainbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), KorActivity.class);
+                startActivity(intent);
+            }
+        });
+        kfeebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), KorFeeActivity.class);
                 startActivity(intent);
             }
         });
