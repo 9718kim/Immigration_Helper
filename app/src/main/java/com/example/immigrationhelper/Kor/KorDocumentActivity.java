53acd0fa752d8b412d99626fdcab718168adb79b
Kimdeokki <9718kim@naver.com>
2021-05-09 14:50:33 +0900

    Visa 액티비티 압축

--- app/src/main/java/com/example/immigrationhelper/Kor/KorDocumentActivity.java
+++ app/src/main/java/com/example/immigrationhelper/Kor/KorDocumentActivity.java
@@ -3,48 +3,15 @@ package com.example.immigrationhelper.Kor;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.immigrationhelper.Kor.KorVisa.KorA1Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorA2Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorA3Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorB1Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorB2Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorC1Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorC3Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorC4Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorD10Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorD1Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorD2Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorD3Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorD4Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorD5Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorD6Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorD7Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorD8Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorD9Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorE10Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorE1Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorE2Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorE3Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorE4Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorE5Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorE6Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorE7Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorE8Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorE9Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorF1Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorF2Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorF3Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorF5Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorF6Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorG1Activity;
-import com.example.immigrationhelper.Kor.KorVisa.KorH1Activity;
 import com.example.immigrationhelper.R;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class KorDocumentActivity extends AppCompatActivity {
     @Override
@@ -54,41 +21,9 @@ public class KorDocumentActivity extends AppCompatActivity {
 
         ImageButton kmainbtn = (ImageButton) findViewById(R.id.kormainbtn);
         ImageButton ksafebtn = (ImageButton) findViewById(R.id.korsafebtn);
-        Button ka1btn = (Button) findViewById(R.id.kora1btn);
-        Button ka2btn = (Button) findViewById(R.id.kora2btn);
-        Button ka3btn = (Button) findViewById(R.id.kora3btn);
-        Button kb1btn = (Button) findViewById(R.id.korb1btn);
-        Button kb2btn = (Button) findViewById(R.id.korb2btn);
-        Button kc1btn = (Button) findViewById(R.id.korc1btn);
-        Button kc3btn = (Button) findViewById(R.id.korc3btn);
-        Button kc4btn = (Button) findViewById(R.id.korc4btn);
-        Button kd1btn = (Button) findViewById(R.id.kord1btn);
-        Button kd2btn = (Button) findViewById(R.id.kord2btn);
-        Button kd3btn = (Button) findViewById(R.id.kord3btn);
-        Button kd4btn = (Button) findViewById(R.id.kord4btn);
-        Button kd5btn = (Button) findViewById(R.id.kord5btn);
-        Button kd6btn = (Button) findViewById(R.id.kord6btn);
-        Button kd7btn = (Button) findViewById(R.id.kord7btn);
-        Button kd8btn = (Button) findViewById(R.id.kord8btn);
-        Button kd9btn = (Button) findViewById(R.id.kord9btn);
-        Button kd10btn = (Button) findViewById(R.id.kord10btn);
-        Button ke1btn = (Button) findViewById(R.id.kore1btn);
-        Button ke2btn = (Button) findViewById(R.id.kore2btn);
-        Button ke3btn = (Button) findViewById(R.id.kore3btn);
-        Button ke4btn = (Button) findViewById(R.id.kore4btn);
-        Button ke5btn = (Button) findViewById(R.id.kore5btn);
-        Button ke6btn = (Button) findViewById(R.id.kore6btn);
-        Button ke7btn = (Button) findViewById(R.id.kore7btn);
-        Button ke8btn = (Button) findViewById(R.id.kore8btn);
-        Button ke9btn = (Button) findViewById(R.id.kore9btn);
-        Button ke10btn = (Button) findViewById(R.id.kore10btn);
-        Button kf1btn = (Button) findViewById(R.id.korf1btn);
-        Button kf2btn = (Button) findViewById(R.id.korf2btn);
-        Button kf3btn = (Button) findViewById(R.id.korf3btn);
-        Button kf5btn = (Button) findViewById(R.id.korf5btn);
-        Button kf6btn = (Button) findViewById(R.id.korf6btn);
-        Button kg1btn = (Button) findViewById(R.id.korg1btn);
-        Button kh1btn = (Button) findViewById(R.id.korh1btn);
+
+        Map<Integer, Integer> buttonLayoutMap = initializeButtonLayoutMap();
+        initializeButtons(buttonLayoutMap);
 
         kmainbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -104,250 +39,61 @@ public class KorDocumentActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        ka1btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorA1Activity.class);
-                startActivity(intent);
-            }
-        });
-        ka2btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorA2Activity.class);
-                startActivity(intent);
-            }
-        });
-        ka3btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorA3Activity.class);
-                startActivity(intent);
-            }
-        });
-        kb1btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorB1Activity.class);
-                startActivity(intent);
-            }
-        });
-        kb2btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorB2Activity.class);
-                startActivity(intent);
-            }
-        });
-        kc1btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorC1Activity.class);
-                startActivity(intent);
-            }
-        });
-        kc3btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorC3Activity.class);
-                startActivity(intent);
-            }
-        });
-        kc4btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorC4Activity.class);
-                startActivity(intent);
-            }
-        });
-        kd1btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorD1Activity.class);
-                startActivity(intent);
-            }
-        });
-        kd2btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorD2Activity.class);
-                startActivity(intent);
-            }
-        });
-        kd3btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorD3Activity.class);
-                startActivity(intent);
-            }
-        });
-        kd4btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorD4Activity.class);
-                startActivity(intent);
-            }
-        });
-        kd5btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorD5Activity.class);
-                startActivity(intent);
-            }
-        });
-        kd6btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorD6Activity.class);
-                startActivity(intent);
-            }
-        });
-        kd7btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorD7Activity.class);
-                startActivity(intent);
-            }
-        });
-        kd8btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorD8Activity.class);
-                startActivity(intent);
-            }
-        });
-        kd9btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorD9Activity.class);
-                startActivity(intent);
-            }
-        });
-        kd10btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorD10Activity.class);
-                startActivity(intent);
-            }
-        });
-        ke1btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorE1Activity.class);
-                startActivity(intent);
-            }
-        });
-        ke2btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorE2Activity.class);
-                startActivity(intent);
-            }
-        });
-        ke3btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorE3Activity.class);
-                startActivity(intent);
-            }
-        });
-        ke4btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorE4Activity.class);
-                startActivity(intent);
-            }
-        });
-        ke5btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorE5Activity.class);
-                startActivity(intent);
-            }
-        });
-        ke6btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorE6Activity.class);
-                startActivity(intent);
-            }
-        });
-        ke7btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorE7Activity.class);
-                startActivity(intent);
-            }
-        });
-        ke8btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorE8Activity.class);
-                startActivity(intent);
-            }
-        });
-        ke9btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorE9Activity.class);
-                startActivity(intent);
-            }
-        });
-        ke10btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorE10Activity.class);
-                startActivity(intent);
-            }
-        });
-        kf1btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorF1Activity.class);
-                startActivity(intent);
-            }
-        });
-        kf2btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorF2Activity.class);
-                startActivity(intent);
-            }
-        });
-        kf3btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorF3Activity.class);
-                startActivity(intent);
-            }
-        });
-        kf5btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorF5Activity.class);
-                startActivity(intent);
-            }
-        });
-        kf6btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorF6Activity.class);
-                startActivity(intent);
-            }
-        });
-        kg1btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorG1Activity.class);
-                startActivity(intent);
-            }
-        });
-        kh1btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), KorH1Activity.class);
-                startActivity(intent);
-            }
-        });
+    }
+
+    private Map<Integer, Integer> initializeButtonLayoutMap() {
+        Map<Integer, Integer> buttonLayoutMap = new HashMap<>();
+
+        buttonLayoutMap.put(R.id.kora1btn, R.layout.kora1);
+        buttonLayoutMap.put(R.id.kora2btn, R.layout.kora2);
+        buttonLayoutMap.put(R.id.kora3btn, R.layout.kora3);
+        buttonLayoutMap.put(R.id.korb1btn, R.layout.korb1);
+        buttonLayoutMap.put(R.id.korb2btn, R.layout.korb2);
+        buttonLayoutMap.put(R.id.korc1btn, R.layout.korc1);
+        buttonLayoutMap.put(R.id.korc3btn, R.layout.korc3);
+        buttonLayoutMap.put(R.id.korc4btn, R.layout.korc4);
+        buttonLayoutMap.put(R.id.kord1btn, R.layout.kord1);
+        buttonLayoutMap.put(R.id.kord2btn, R.layout.kord2);
+        buttonLayoutMap.put(R.id.kord3btn, R.layout.kord3);
+        buttonLayoutMap.put(R.id.kord4btn, R.layout.kord4);
+        buttonLayoutMap.put(R.id.kord5btn, R.layout.kord5);
+        buttonLayoutMap.put(R.id.kord6btn, R.layout.kord6);
+        buttonLayoutMap.put(R.id.kord7btn, R.layout.kord7);
+        buttonLayoutMap.put(R.id.kord8btn, R.layout.kord8);
+        buttonLayoutMap.put(R.id.kord9btn, R.layout.kord9);
+        buttonLayoutMap.put(R.id.kord10btn, R.layout.kord10);
+        buttonLayoutMap.put(R.id.kore1btn, R.layout.kore1);
+        buttonLayoutMap.put(R.id.kore2btn, R.layout.kore2);
+        buttonLayoutMap.put(R.id.kore3btn, R.layout.kore3);
+        buttonLayoutMap.put(R.id.kore4btn, R.layout.kore4);
+        buttonLayoutMap.put(R.id.kore5btn, R.layout.kore5);
+        buttonLayoutMap.put(R.id.kore6btn, R.layout.kore6);
+        buttonLayoutMap.put(R.id.kore7btn, R.layout.kore7);
+        buttonLayoutMap.put(R.id.kore8btn, R.layout.kore8);
+        buttonLayoutMap.put(R.id.kore9btn, R.layout.kore9);
+        buttonLayoutMap.put(R.id.kore10btn, R.layout.kore10);
+        buttonLayoutMap.put(R.id.korf1btn, R.layout.korf1);
+        buttonLayoutMap.put(R.id.korf2btn, R.layout.korf2);
+        buttonLayoutMap.put(R.id.korf3btn, R.layout.korf3);
+        buttonLayoutMap.put(R.id.korf5btn, R.layout.korf5);
+        buttonLayoutMap.put(R.id.korf6btn, R.layout.korf6);
+        buttonLayoutMap.put(R.id.korg1btn, R.layout.korg1);
+        buttonLayoutMap.put(R.id.korh1btn, R.layout.korh1);
+
+        return buttonLayoutMap;
+    }
+
+    private void initializeButtons(Map<Integer, Integer> buttonLayoutMap) {
+        for (Integer key : buttonLayoutMap.keySet()) {
+            ImageButton baseButton = (ImageButton) findViewById(key);
+            baseButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(getApplicationContext(), BaseKorActivity.class);
+                    intent.putExtra("layout", buttonLayoutMap.get(key));
+                    startActivity(intent);
+                }
+            });
+        }
     }
 }
--- app/src/main/java/com/example/immigrationhelper/Kor/KorGuideActivity.java
+++ app/src/main/java/com/example/immigrationhelper/Kor/KorGuideActivity.java
@@ -1,6 +1,7 @@
 package com.example.immigrationhelper.Kor;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -17,12 +18,36 @@ public class KorGuideActivity extends AppCompatActivity {
         setContentView(R.layout.kor_guide);
 
         ImageButton kmainbtn = (ImageButton) findViewById(R.id.kormainbtn);
+        ImageButton kcall1 = (ImageButton) findViewById(R.id.korcallbtn1);
+        ImageButton kcall2 = (ImageButton) findViewById(R.id.korcallbtn2);
+        ImageButton kcall3 = (ImageButton) findViewById(R.id.korcallbtn3);
 
         kmainbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), KorActivity.class);
                 startActivity(intent);
+            }
+        });
+        kcall1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:02-2110-3000"));
+                startActivity(i);
+            }
+        });
+        kcall2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i2 = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:1345"));
+                startActivity(i2);
+            }
+        });
+        kcall3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i3 = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:82-2-6908-1345"));
+                startActivity(i3);
             }
         });
     }
