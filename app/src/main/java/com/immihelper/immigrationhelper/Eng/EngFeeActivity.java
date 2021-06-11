66265c45987cdfeb6698da285dc9f119e2b0a573
Kimdeokki <9718kim@naver.com>
2021-06-11 22:29:05 +0900

    유의사항, 수수료 아이콘 수정

---
+++ app/src/main/java/com/immihelper/immigrationhelper/Eng/EngFeeActivity.java
@@ -0,0 +1,29 @@
+package com.immihelper.immigrationhelper.Eng;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.immihelper.immigrationhelper.R;
+
+public class EngFeeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.eng_fee);
+
+        ImageButton emainbtn = (ImageButton) findViewById(R.id.engmainbtn);
+
+        emainbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), EngActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
